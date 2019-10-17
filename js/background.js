35825e0c664d9040852ed89eef8347e92da36d48
Elias Keller <elias.keller@abraxas.ch>
2019-10-17 17:06:28 +0200

    feat: rounds funktioniert jetzt...jedoch html noch gecleart werden

--- js/background.js
+++ js/background.js
@@ -2,47 +2,53 @@
   var rounds = []
   var selectedRound = 3;
 
-  new Vue({
+var fixturesComponent = new Vue({
     el: '#v-for-object',
     data: {
       object: this.fixtures
-    },
+    },  
     created: function () {
-
-      fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/Regular_Season_-_" + selectedRound, {
+      this.loadFixtures();
+    },
+    methods: {
+      loadFixtures: function(){
+        fetch("https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/Regular_Season_-_" + selectedRound, {
 	          "method": "GET",
             "headers": {
               "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
               "x-rapidapi-key": "b3f656f046mshe5362581d8dedb8p115949jsn1bb5e19947d0"
+              }
+        })
+        .then(
+          function(response) {
+            if (response.status !== 200) {
+              console.log('Looks like there was a problem. Status Code: ' +
+                response.status);
+              return;
             }
-      })
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-    
-          response.json().then(function(data) {
-            console.log(data)
-            data.api.fixtures.forEach(function (fixture) {
-              let date = new Date(Date.parse(fixture.event_date));
-              fixtures.push({
-                eventDate: date.toDateString() + ' ' + date.getHours() + ':' + date.getMinutes(),
-                homeTeamLogo: fixture.homeTeam.logo,
-                homeTeamName: fixture.homeTeam.team_name,
-                awayTeamLogo: fixture.awayTeam.logo,
-                awayTeamName: fixture.awayTeam.team_name,
+            response.json().then(function(data) {
+              data.api.fixtures.forEach(function (fixture) {
+                let date = new Date(Date.parse(fixture.event_date));
+                fixtures.push({
+                  eventDate: date.toDateString() + ' ' + date.getHours() + ':' + date.getMinutes(),
+                  homeTeamLogo: fixture.homeTeam.logo,
+                  homeTeamName: fixture.homeTeam.team_name,
+                  awayTeamLogo: fixture.awayTeam.logo,
+                  awayTeamName: fixture.awayTeam.team_name,
+                });
               });
             });
-          });
+            console.log('2', this.fixtures);
+          }
+        )
+        .catch(function(err) {
+          console.log('Fetch Error :-S', err);
+        });
+            
         }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-      });
     }
+
+
   });
 
 
@@ -76,10 +82,13 @@
         }
       )
       .catch(function(err) {
-        console.log('Fetch Error :-S', err);
+        console.log('Fetch Error:', err);
       });
+    },
+    methods: {
+      switchRound: function(value){
+          selectedRound = value;
+          fixturesComponent.loadFixtures();
+      }
     }
   });
-
-
-
